6c77fbea84fccd7a4ad78fba0d6c5116ad274ef7
jong129 <144347847+jong129@users.noreply.github.com>
2024-12-13 20:53:46 +0900

    2024-12-07

--- app/src/main/java/com/example/myapplication/api/responses/ChangePasswordResponse.java
+++ app/src/main/java/com/example/myapplication/api/responses/ChangePasswordResponse.java
@@ -1,7 +1,17 @@
 package com.example.myapplication.api.responses;
 
-public class CheckIDResponse {
+public class ChangePasswordResponse {
+    private boolean success; // 요청 성공 여부
     private String message;  // 서버에서 반환하는 메시지
+
+    // Getter 및 Setter
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
 
     public String getMessage() {
         return message;
