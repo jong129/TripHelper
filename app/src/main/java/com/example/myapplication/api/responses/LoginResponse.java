4cdf5a491d2170478431804a9379d56ef7bfa5d7
jong129 <144347847+jong129@users.noreply.github.com>
2025-01-10 00:31:55 +0900

    25/1/10

--- app/src/main/java/com/example/myapplication/api/responses/LoginResponse.java
+++ app/src/main/java/com/example/myapplication/api/responses/LoginResponse.java
@@ -3,7 +3,9 @@ package com.example.myapplication.api.responses;
 public class LoginResponse {
     private boolean success;
     private String message;
+    private String token; // 서버에서 전달받는 토큰
 
+    // Getter와 Setter 추가
     public boolean isSuccess() {
         return success;
     }
@@ -18,5 +20,13 @@ public class LoginResponse {
 
     public void setMessage(String message) {
         this.message = message;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
     }
 }
